e2f6325fe7b17e452b752dd2c7de651affc428b1
Hadrien Belleville <85734187+vraus@users.noreply.github.com>
2022-12-10 16:17:32 -0500

    session.php created Trying to a php local file but not working ...

---
+++ session.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Session</title>
+    <link href="css/styles.css" rel="stylesheet" type="text/css" />
+    <script type="text/javascript" src="js/main.js"></script>
+</head>
+
+<body>
+    <header id="header">
+        <h1>SESSION</h1>
+        <p>Page de session</p>
+    </header>
+
+            <section>
+                <footer>
+                    <div></div>
+                    <div>Hadrien BELLEVILLE - <a href="https://www.linkedin.com/in/b%C3%A9atrice-garcia-cegarra-bb6a841a2/">Béatrice GARCIA CEGARRA</a></div>
+                    <div>Travail Pratique #3</div>
+                    <div><a href="https://cours.uqac.ca/8INF138">8INF138 - Sécurité des Réseaux et du Web</a></div>
+                </footer>
+            </section>
+    </body>
+</html>
