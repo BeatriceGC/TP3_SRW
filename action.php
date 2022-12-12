5c840b2f9fcd3a65b4fcc0ca0ab74b812e247dbf
Hadrien Belleville <85734187+vraus@users.noreply.github.com>
2022-12-12 16:29:48 -0500

    creation du début de la création de compte.

--- action.php
+++ action.php
@@ -3,26 +3,37 @@
 <header></header>
 <?php
     session_start();
-    $id = htmlspecialchars($_POST['email']);
+    $id = "pouet";
+    $email = htmlspecialchars($_POST['email']);
     $mdp = htmlspecialchars($_POST['password']);
+    $mdp2 = htmlspecialchars($_POST['password2']);
+
     $file = file_get_contents("data2.json");
+    $users = json_decode($file, true);
 
-    $users = json_decode($file);
-
-    foreach($users as $usr){
-        // Test des identifiants dans la bdd
-        if($usr->email == $id) {
-            if($usr->password == $mdp){
-                // la connexion est réussie
-                $_SESSION['login'] = $usr->name;
-                $_SESSION['role'] = $usr->role;
-                header("Location:html/admin.php");
-                echo("<h1>");
+if (empty($mdp2)){
+        // Utilisateur veut se connecter
+        foreach($users as $usr){
+            // Test des identifiants dans la bdd
+            if($usr->email == $email) {
+                if($usr->password == $mdp){
+                    // la connexion est réussie
+                    $_SESSION['login'] = $usr->name;
+                    $_SESSION['role'] = $usr->role;
+                    header("Location:html/admin.php");
+                    echo("<h1>");
                     echo("Welcomenn, " . $usr->name . ".");
-                echo("</h1>");
-            } else echo "unknown password\n";
-        } else echo "unknown name\n;";
+                    echo("</h1>");
+                } else echo "unknown password\n";
+            } else echo "unknown name\n;";
+        }
+    } else {
+        // Utilisateur veut s'inscrire
+        $users[] = '{\"name\" : '.$id.',\"email\" : '.$email.',\"password\" : '.$mdp.',\"role\" : \"none\",}';
+        $file = json_encode($users);
+        file_put_contents('data2.json', $file);
     }
+
 ?>
 <body></body>
 </html>
