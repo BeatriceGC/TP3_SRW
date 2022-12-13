b0a36c0a5574a982a503fb581819688df3c00300
XybyaHearth <117529808+BeatriceGC@users.noreply.github.com>
2022-12-12 21:54:04 -0500

    Modifications of the signup wrapper

--- js/mainIndex.js
+++ js/mainIndex.js
@@ -6,9 +6,15 @@ function switchMode() {
     {
         document.getElementById("switch_span").innerText = "se connecter"
         document.getElementById("validate").value = "S'inscrire";
+
         document.getElementById("confirm_password").style.display = "grid";
+        document.getElementById("divName").style.display = "grid";
         document.getElementById("complexity").style.display = "grid";
+        document.getElementById("radioButtons").style.display = "grid";
+
         document.getElementById("complexity").style.visibility = "hidden";
+        document.getElementById("wrapper").style.overflowY = "scroll";
+
         document.getElementById("myForm").onsubmit = signUp;
         mode = "signup";
     }
@@ -16,8 +22,14 @@ function switchMode() {
     {
         document.getElementById("switch_span").innerText = "s'inscrire"
         document.getElementById("validate").value = "Se connecter";
+
         document.getElementById("confirm_password").style.display = "none";
+        document.getElementById("divName").style.display = "grid";
         document.getElementById("complexity").style.display = "none";
+        document.getElementById("radioButtons").style.display = "none";
+
+        document.getElementById("wrapper").style.overflowY = "hidden";
+
         document.getElementById("myForm").onsubmit = logIn;
         mode = "login";
     }
